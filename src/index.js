74a22e10c8a84b8a8c849615af309d05d33fdd69
João Pedro <joaopjt@icloud.com>
2023-09-13 12:39:11 -0300

    added c() method values

--- src/index.js
+++ src/index.js
@@ -180,7 +180,7 @@ class Placard {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['water'],
       web: []
     };
     
@@ -196,19 +196,19 @@ class Placard {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bow'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['mouth'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
       }
     }
