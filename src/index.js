f39f7191ee8d77bb35bf9ff3515a0af911103732
João Pedro <joaopjt@icloud.com>
2023-09-14 12:43:48 -0300

    added x() method values

--- src/index.js
+++ src/index.js
@@ -1012,7 +1012,7 @@ class Placard {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['one'],
       web: []
     };
     
@@ -1028,11 +1028,11 @@ class Placard {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
       }
     }
