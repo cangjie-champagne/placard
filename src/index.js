be16a004f4531bd87b493191fef29ea58e9fff02
João Pedro <joaopjt@icloud.com>
2023-09-14 12:24:46 -0300

    added q() method values

--- src/index.js
+++ src/index.js
@@ -732,7 +732,7 @@ class Placard {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['moon'],
       web: []
     };
     
@@ -748,19 +748,19 @@ class Placard {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bow'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['moon'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['predict'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['field'];
           break;
       }
     }
