e70ab25068dbf827128e69199297148120d4f1ff
João Pedro <joaopjt@icloud.com>
2023-09-14 12:55:16 -0300

    added z() method values

--- src/index.js
+++ src/index.js
@@ -1084,7 +1084,7 @@ class Placard {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['female'],
       web: []
     };
     
@@ -1100,19 +1100,19 @@ class Placard {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['water'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['predict'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['dagger-axe'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
       }
     }
